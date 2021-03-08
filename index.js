a31154eabed0a81179b5d119ea32c0ab239f1a5e
Philip Oreste <philiporeste2@gmail.com>
2021-03-08 11:13:41 -0500

    March8

--- index.js
+++ index.js
@@ -13,12 +13,35 @@ var computerResponse = Math.round(Math.random() * (2-0) + 0); // 0-2
 var choices = ["Rock", "Paper", "Scissors"]
 console.log (choices[computerResponse])
 
-
-//Math.round(Math.random() * (20-10) + 10) => 10
-//10 - 20
-
-// 0 -> 1 0.6
-
-
 // 4. Compare the responses and see who is the winner
+//Rock => stronger than scissors and weaker than paper
+//Scissors => stronger than paper and weaker than rock
+//Paper  => stronger than rock and weaker than scissors
+// User picked rock
+// Computer picked paper
+// What is rock weak to? Paper
+// What is paper weak to? Scissors
+// Is Rock weak to Computer? Yes
+// Is Paper weak to User? No
+// comparator[choices[computerResponse]] //comparator["Rock"] => {strongTo:"Scissors", weakTo:"Rock"}
+
+var comparator = {
+    Rock: {
+        strongTo: "scissors",
+        weakTo: "paper",
+    },
+    Scissors: {
+        strongTo: "paper",
+        weakTo: "rock",
+    },
+    Paper: {
+        strongTo: "rock",
+        weakTo: "scissors",
+    }
+}
+console.log(comparator[userResponse]) // {strongTo:"", weakTo:""}
+console.log(comparator[choices[computerResponse]]) //
+
+
+
 // 5. Declare win or draw
